750a6e13eab17fc9c18ad61f040b64b4512f6425
Socdaaldev <socdaaldev1@gmail.com>
2018-02-16 22:09:28 -0500

    Updated to the latest packages

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -2,24 +2,21 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
+import compression from 'compression';
 //var path = require('path');
 //var open = require('open');
 
 const port = 3000;
 const app = express();
-const compiler= webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+app.use(compression());
+app.use(express.static('dist'));
+
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 
 });
-
+/*
 app.get('/users', function(req, res) {
   // Hard coding for asimplicity. pretend this hits a real database
   res.json([
@@ -29,7 +26,7 @@ app.get('/users', function(req, res) {
     {"id": 1, "firstName":"Socdaal-4", "lastName":"Muse", "email":"socdaal@gmail.com"}
   ]);
 });
-
+*/
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
---
+++ src/vendor.js
@@ -0,0 +1,9 @@
+/* This file ocntains references to the vendor libraries 
+w're using in this project. This is used by webpack 
+in the production build only*.
+
+*/
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
